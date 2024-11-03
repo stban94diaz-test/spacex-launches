b6aa4836048ee793e0170b809b3360c5ef1f4a30
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2024-11-03 18:28:15 -0500

    :construction: Configure GitHub page - Node adapter for Server-side Rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,15 @@ import preact from '@astrojs/preact';
 
 import { BASE_PROJECT } from './src/constants/general';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
   base: BASE_PROJECT,
   site: 'https://stban94diaz.github.io',
 });
