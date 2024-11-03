ec72211cfc85e1ea3e007e313be3b4982ae4a68a
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2024-11-03 17:41:51 -0500

    :construction: Crate a client component with preact

---
+++ src/components/YouLikeThis.tsx
@@ -0,0 +1,23 @@
+import { useState } from "preact/hooks"
+
+export const YouLikeThis = () => {
+  const [count, setCount] = useState(0)
+
+  const handleIncrease = () => setCount(count => count + 1)
+  const handleDecrease = () => {
+    if (count > 0) setCount(count => count - 1)
+  }
+
+  return (
+    <section class='flex justify-center items-center gap-4 mb-4'>
+      <span class='text-yellow-300'>You like this X{count}</span>
+      <button onClick={handleIncrease} type="button" class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M6.979 3.074a6 6 0 0 1 4.988 1.425l.037 .033l.034 -.03a6 6 0 0 1 4.733 -1.44l.246 .036a6 6 0 0 1 3.364 10.008l-.18 .185l-.048 .041l-7.45 7.379a1 1 0 0 1 -1.313 .082l-.094 -.082l-7.493 -7.422a6 6 0 0 1 3.176 -10.215z" /></svg>
+        <span class="sr-only">Icon heard</span>
+      </button>
+      <button onClick={handleDecrease} type="button" class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:focus:ring-blue-800 dark:hover:bg-blue-500">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" /></svg>
+      </button>
+    </section>
+  )
+}
