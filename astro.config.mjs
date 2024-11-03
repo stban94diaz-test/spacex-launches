c3fb736277530c099f5a7c2d4060df7b22609c57
Esteban Danilo Diaz Riascos <stban94diaz@gmail.com>
2024-11-03 18:14:47 -0500

    :construction: Configure GitHub page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import { BASE_PROJECT } from './src/constants/general';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   output: 'hybrid',
+  base: BASE_PROJECT,
+  site: 'https://stban94diaz.github.io',
 });
